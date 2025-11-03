2a954101628714359c91827082bbc305f19e2097
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-11-03 23:00:13 +0600

    update

--- database/migrations/2025_11_03_150711_create_carts_table.php
+++ database/migrations/2025_11_03_150711_create_carts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')
+                ->nullable()
                 ->constrained('users')
                 ->onDelete('cascade');
             $table->string('session_cart_id')->nullable();
