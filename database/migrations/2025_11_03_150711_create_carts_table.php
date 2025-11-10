8ffad800701fe22a85b58efc84334a8aca526f92
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-11-10 09:21:57 +0600

    update migration for carts, cart_items, orders, orders_items

--- database/migrations/2025_11_03_150711_create_carts_table.php
+++ database/migrations/2025_11_03_150711_create_carts_table.php
@@ -13,17 +13,14 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+
             $table->foreignId('user_id')
                 ->nullable()
                 ->constrained('users')
                 ->onDelete('cascade');
-            $table->string('session_cart_id')->nullable();
-            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
-            $table->unsignedInteger('quantity')->default(1);
-            $table->decimal('shipping_price', 10, 2)->nullable();
-            $table->decimal('tax_price', 10, 2)->nullable();
-            $table->decimal('item_price', 10, 2)->nullable();
-            $table->decimal('total_price', 10, 2)->nullable();
+
+            // Session ID for guest users
+            $table->string('session_cart_id')->nullable()->unique();
 
             $table->timestamps();
         });
