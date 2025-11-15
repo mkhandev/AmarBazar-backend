0d8aaed34c79b40ab6d2a80fb0e1b4468c7498e2
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-11-15 15:51:40 +0600

    update seeder

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -18,9 +18,11 @@ class ReviewFactory extends Factory
         return [
             //'product_id'  => Product::inRandomOrder()->first()->id ?? Product::factory(),
             //'user_id'     => User::inRandomOrder()->first()->id ?? User::factory(),
-            'title'       => $this->faker->sentence(4),
+            //'title'       => $this->faker->sentence(4),
+            'title'       => $this->faker->firstName() . "'s Review",
             'rating'      => $this->faker->randomFloat(2, 1, 5), // e.g. 4.50
-            'comment'     => $this->faker->paragraph(),
+                                                                 //'comment'     => $this->faker->paragraph(),
+            'comment'     => $this->faker->sentences(rand(3, 6), true),
             'is_approved' => true,
         ];
     }
